cbf112fe25eac127d96525dc6d507dba99a5017d
Alex <shshsh714@gmail.com>
2021-02-17 00:20:45 +1100

    remove to-the-top

--- teample/src/components/Footer/Footer.js
+++ teample/src/components/Footer/Footer.js
@@ -10,7 +10,6 @@ function Footer() {
             <img src={logo} width="auto" height="60" alt="" loading="lazy" />
             <p>Copyright &copy; {(new Date().getFullYear())} Teample. All rights reserved.</p>
             </footer>
-            <Link id="back-to-top" href="#" class="btn btn-light btn-outline-info back-to-top rounded-circle" role="button"><i class="bi bi-chevron-up"></i></Link>
         </div>
     )
 }
