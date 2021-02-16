b4f058c873bc11ae5ada77939d07a7fc0f87eeef
Alex <shshsh714@gmail.com>
2021-02-17 01:31:17 +1100

    login

--- teample/src/pages/Login.js
+++ teample/src/pages/Login.js
@@ -5,26 +5,26 @@ import Exlog from '../components/Login/Exlog';
 function Login() {
     return (
         <div className="mx-auto" style={{width: "450px"}}>
-            <div class="card text-center">
-                <h2 class="card-title">Login</h2>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <div class="container">
+            <div className="card text-center">
+                <h2 className="card-title">Login</h2>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <div className="container">
                     <form>
-                    <div class="form-group login">
-                        <input type="text" class="form-control" id="exampleInputEmail" placeholder="Username or Email" />
+                    <div className="form-group login">
+                        <input type="text" className="form-control" id="exampleInputEmail" placeholder="Username or Email" />
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" />
+                    <div className="form-group">
+                        <input type="password" className="form-control" id="exampleInputPassword" placeholder="Password" />
                     </div>
-                    <div class="form-group">
-                        <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="dropdownCheck2" />
-                        <label class="form-check-label" for="dropdownCheck2">
+                    <div className="form-group">
+                        <div className="form-check">
+                        <input type="checkbox" className="form-check-input" id="dropdownCheck2" />
+                        <label className="form-check-label" for="dropdownCheck2">
                             Remember me
                         </label>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-dark w-100">Lets go!</button>
+                    <button type="submit" className="btn btn-dark w-100">Lets go!</button>
                     </form>
                 </div>
                 <Exlog />
